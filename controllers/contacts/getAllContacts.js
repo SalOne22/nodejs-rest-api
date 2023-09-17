417c4d096e5b2dce4f88daaecd15541674af7355
Vladyslav Vladinov <111443297+SalOne22@users.noreply.github.com>
2023-09-17 19:07:27 +0200

    Added pagination

--- controllers/contacts/getAllContacts.js
+++ controllers/contacts/getAllContacts.js
@@ -1,7 +1,13 @@
 const { contactsModel } = require('../../models');
 
 async function getAll(req, res) {
-  const result = await contactsModel.find({ owner: req.user.id });
+  const { page = 1, limit = 10 } = req.query;
+  const skip = (page - 1) * limit;
+
+  const result = await contactsModel
+    .find({ owner: req.user.id })
+    .skip(skip)
+    .limit(limit);
 
   res.json(result);
 }
