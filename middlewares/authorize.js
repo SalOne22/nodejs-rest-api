7f6d2d0392eed49b27127745825f10b7ca61b82d
Vladyslav Vladinov <111443297+SalOne22@users.noreply.github.com>
2023-09-18 09:36:02 +0200

    Added more error messages

--- middlewares/authorize.js
+++ middlewares/authorize.js
@@ -9,11 +9,12 @@ const { SECRET } = process.env;
 const authorize = async (req, res, next) => {
   const { authorization } = req.headers;
 
-  if (!authorization) throw new HttpError(401);
+  if (!authorization) throw new HttpError(401, 'Missing Authorization header');
 
   const [bearer, token] = authorization.split(' ');
 
-  if (bearer !== 'Bearer' || !token) throw new HttpError(401);
+  if (bearer !== 'Bearer' || !token)
+    throw new HttpError(401, 'Missing "Bearer" string or token');
 
   const { id } = jwt.verify(token, SECRET);
   const user = await usersModel.findById(id);
