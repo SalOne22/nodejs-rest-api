fc7d95f60c3316a9c2d1fd76f678c30ad44fb3bc
Vladyslav Vladinov <111443297+SalOne22@users.noreply.github.com>
2023-10-01 22:07:03 +0200

    Renamed NODEMAILER env to META_UA

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -1,21 +1,21 @@
 const nodemailer = require('nodemailer');
 
-const { NODEMAILER_EMAIL_FROM, NODEMAILER_EMAIL_PASSWORD } = process.env;
+const { META_UA_EMAIL_FROM, META_UA_EMAIL_PASSWORD } = process.env;
 
 const config = {
   host: 'smtp.meta.ua',
   port: 465,
   secure: true,
   auth: {
-    user: NODEMAILER_EMAIL_FROM,
-    pass: NODEMAILER_EMAIL_PASSWORD,
+    user: META_UA_EMAIL_FROM,
+    pass: META_UA_EMAIL_PASSWORD,
   },
 };
 
 const transport = nodemailer.createTransport(config);
 
 const sendEmail = (data) => {
-  const email = { ...data, from: NODEMAILER_EMAIL_FROM };
+  const email = { ...data, from: META_UA_EMAIL_FROM };
   return transport.sendMail(email);
 };
 
